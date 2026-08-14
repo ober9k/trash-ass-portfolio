92cb0b6d58b968c25922b109632471d03fae3d22
Dean Hardaker <ober9k@users.noreply.github.com>
2026-08-14 22:33:33 +1000

    Add Tailwind CSS support

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,13 @@ import './App.css'
 function App() {
   return (
     <>
-      <section>
-        Trash Ass Portfolio.
+      <section className="m-2 p-2">
+        <h1 className="text-xl">
+          Trash Ass Portfolio.
+        </h1>
+        <p>
+          Experimental app for some cryptocurrency portfolio tracking as apps like Blockfolio and Delta aren't too great nowadays.
+        </p>
       </section>
     </>
   );
