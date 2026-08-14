6e7168968929247b227db23af9cfcbad51ed1757
Dean Hardaker <ober9k@users.noreply.github.com>
2026-08-14 23:32:19 +1000

    Add mock transactions data

---
+++ src/data/mock/transactions.ts
@@ -0,0 +1,33 @@
+const symbols = {
+  HYPE: "hype",
+  XLM:  "xlm",
+} as const;
+
+export const transactions = [
+  // HYPE
+  { symbol: symbols.HYPE, quantity:  0.04034861, price:  61.34653771, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-05-07T22:47:00+10:00") },
+  { symbol: symbols.HYPE, quantity:  0.04181301, price:  59.19802293, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-05-15T00:02:00+10:00") },
+  { symbol: symbols.HYPE, quantity:  0.02788311, price:  88.77228992, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-05-22T00:31:00+10:00") },
+  { symbol: symbols.HYPE, quantity:  0.02514584, price:  98.43566668, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-05-31T20:55:00+10:00") },
+  { symbol: symbols.HYPE, quantity:  0.02353384, price: 105.17822526, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-06-04T00:11:00+10:00") },
+  { symbol: symbols.HYPE, quantity:  0.03004085, price:  82.39605486, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-06-10T21:28:00+10:00") },
+  { symbol: symbols.HYPE, quantity:  0.02405696, price: 102.89111861, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-06-19T00:50:00+10:00") },
+  { symbol: symbols.HYPE, quantity:  0.02660140, price:  93.04952088, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-06-24T22:50:00+10:00") },
+  { symbol: symbols.HYPE, quantity:  0.02624396, price:  94.31684566, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-07-01T22:12:00+10:00") },
+  { symbol: symbols.HYPE, quantity:  0.02496006, price:  99.16833232, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-07-11T15:12:00+10:00") },
+  { symbol: symbols.HYPE, quantity:  0.02549459, price:  97.08912851, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-07-16T21:07:00+10:00") },
+  { symbol: symbols.HYPE, quantity:  0.02797672, price:  88.47525817, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-07-23T23:22:00+10:00") },
+  { symbol: symbols.HYPE, quantity:  0.03122658, price:  79.26732690, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-07-30T21:05:00+10:00") },
+  { symbol: symbols.HYPE, quantity:  0.03020418, price:  81.95049575, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-08-07T00:54:00+10:00") },
+  { symbol: symbols.HYPE, quantity:  0.02903263, price:  85.25743361, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-08-14T00:29:00+10:00") },
+  // XLM
+  { symbol: symbols.XLM,  quantity:  6.82780281, price:   0.36252475, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-06-19T00:51:00+10:00") },
+  { symbol: symbols.XLM,  quantity:  8.65977810, price:   0.28583267, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-06-24T22:50:00+10:00") },
+  { symbol: symbols.XLM,  quantity:  8.31833260, price:   0.29756535, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-07-01T22:12:00+10:00") },
+  { symbol: symbols.XLM,  quantity:  8.78206772, price:   0.28185248, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-07-11T15:12:00+10:00") },
+  { symbol: symbols.XLM,  quantity:  8.94787326, price:   0.27662970, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-07-16T21:07:00+10:00") },
+  { symbol: symbols.XLM,  quantity:  9.17825268, price:   0.26968614, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-07-23T23:23:00+10:00") },
+  { symbol: symbols.XLM,  quantity:  9.73721211, price:   0.25420495, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-07-30T21:05:00+10:00") },
+  { symbol: symbols.XLM,  quantity: 10.49533797, price:   0.23584257, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-08-07T00:55:00+10:00") },
+  { symbol: symbols.XLM,  quantity: 10.59447731, price:   0.23363564, fee: 0.02475248, total: 2.50000000, purchasedAt: new Date("2026-08-14T00:29:00+10:00") },
+];
