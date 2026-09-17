ad1a3e2d91c780e9554fe03fc6feba01e253a299
Dean Hardaker <ober9k@users.noreply.github.com>
2026-09-17 23:55:04 +1000

    Add property for transaction type and type for transaction total

--- shared/src/types/transaction.ts
+++ shared/src/types/transaction.ts
@@ -1,5 +1,13 @@
 import { SymbolType } from "./token";
 
+export const TransactionType = {
+  Buy:      "buy",
+  Sell:     "sell",
+  Transfer: "transfer",
+}
+
+export type TransactionType = typeof TransactionType[keyof typeof TransactionType];
+
 export type Transaction = {
   symbol:      SymbolType,
   quantity:    number,
@@ -7,4 +15,17 @@ export type Transaction = {
   fee:         number,
   total:       number,
   purchasedAt: Date,
+  /* tbd for renaming */
+  transactionType: TransactionType,
+};
+
+export type TransactionTotal = {
+  symbol:       SymbolType,
+  quantity:     number,
+  buyAverage:   number,
+  sellAverage:  number,
+  buyTotal:     number,
+  sellTotal:    number,
+  feeTotal:     number,
+  transactions: number,
 };
