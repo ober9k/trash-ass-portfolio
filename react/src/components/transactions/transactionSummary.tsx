6da42f493c99a6266346b7db3144f232b6db56a7
Dean Hardaker <ober9k@users.noreply.github.com>
2026-09-18 22:31:04 +1000

    Add separate property display component

--- react/src/components/transactions/transactionSummary.tsx
+++ react/src/components/transactions/transactionSummary.tsx
@@ -3,6 +3,21 @@ import PercentDisplay from "@/components/utils/percentDisplay.tsx";
 import type { Token } from "@shared/types/token.ts";
 import type { TransactionTotal } from "@shared/types/transaction.ts";
 
+function PropertyDisplay({ title, children }) {
+  return (
+    <>
+      <div className={"grow"}>
+        <h3 className={"py-1 text-xs text-center text-gray-600 font-medium uppercase"}>
+          {title}
+        </h3>
+        <h4 className={"text-center text-sm font-bold"}>
+          {children}
+        </h4>
+      </div>
+    </>
+  );
+}
+
 type Props = {
   transactionTotal: TransactionTotal,
 };
@@ -24,26 +39,22 @@ function TransactionSummary(props: Props) {
         </header>
         <section className={"flex justify-center p-1"}>
           <div className={"grow"}>
-              <h3 className={"py-1 text-xs text-center text-gray-600 font-medium uppercase"}>Total Profit</h3>
-              <h4 className={"text-center text-sm font-bold"}>
-                ${(summary.marketValue - 75 - summary.totalValue).toFixed(2)} USD
-                <PercentDisplay marketValue={summary.marketValue} purchaseValue={summary.totalValue} />
-              </h4>
+            <PropertyDisplay title={"Total Profit"}>
+              ${(summary.marketValue - summary.totalValue).toFixed(2)} USD
+              <PercentDisplay marketValue={summary.marketValue} purchaseValue={summary.totalValue} />
+            </PropertyDisplay>
           </div>
         </section>
         <section className={"flex justify-center p-1"}>
-          <div className={"grow"}>
-            <h3 className={"py-1 text-xs text-center text-gray-600 font-medium uppercase"}>Holdings</h3>
-            <h4 className={"text-center text-sm font-bold"}>{summary.quantity.toFixed(4)} {summary.symbol.toUpperCase()}</h4>
-          </div>
-          <div className={"grow"}>
-            <h3 className={"py-1 text-xs text-center text-gray-600 font-medium uppercase"}>Market Value</h3>
-            <h4 className={"text-center text-sm font-bold"}>${summary.marketValue.toFixed(2)} USD</h4>
-          </div>
-          <div className={"grow"}>
-            <h3 className={"py-1 text-xs text-center text-gray-600 font-medium uppercase"}>Total Cost</h3>
-            <h4 className={"text-center text-sm font-bold"}>${summary.totalValue.toFixed(2)} USD</h4>
-          </div>
+          <PropertyDisplay title={"Holdings"}>
+            {summary.quantity.toFixed(4)} {summary.symbol.toUpperCase()}
+          </PropertyDisplay>
+          <PropertyDisplay title={"Market Value"}>
+             ${summary.marketValue.toFixed(2)} USD
+          </PropertyDisplay>
+          <PropertyDisplay title={"Total Cost"}>
+            ${summary.totalValue.toFixed(2)} USD
+          </PropertyDisplay>
         </section>
       </article>
     </>
